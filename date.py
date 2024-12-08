83fb48b8e535395d103c30b93001cfd0b2e83228
Zoe <github@zoe-codez.tools>
2024-12-08 15:09:41 -0600

    Improved entity syncing (#10)

--- date.py
+++ date.py
@@ -1,28 +1,14 @@
-from .bridge import SynapseBridge
-from .const import DOMAIN
-
-from homeassistant.config_entries import ConfigEntry
-from homeassistant.core import callback, HomeAssistant
-from datetime import date
-from homeassistant.const import EntityCategory
-from homeassistant.helpers.device_registry import DeviceInfo
-from homeassistant.helpers.entity_platform import AddEntitiesCallback
-from homeassistant.components.date import DateEntity
 import logging
 
+from datetime import date
+from homeassistant.components.date import DateEntity
+from homeassistant.config_entries import ConfigEntry
+from homeassistant.core import callback, HomeAssistant
+from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-class SynapseDateDefinition:
-    attributes: object
-    device_class: str
-    entity_category: str
-    icon: str
-    unique_id: str
-    name: str
-    state: str | int
-    suggested_object_id: str
-    supported_features: int
-    translation_key: str
-
+from .synapse.base_entity import SynapseBaseEntity
+from .synapse.bridge import SynapseBridge
+from .synapse.const import DOMAIN, SynapseDateDefinition
 
 async def async_setup_entry(
     hass: HomeAssistant,
@@ -31,78 +17,20 @@ async def async_setup_entry(
 ) -> None:
     """Setup the router platform."""
     bridge: SynapseBridge = hass.data[DOMAIN][config_entry.entry_id]
-    entities = bridge.config_entry.get("date")
+    entities = bridge.app_data.get("date")
     if entities is not None:
         async_add_entities(SynapseDate(hass, bridge, entity) for entity in entities)
 
-
-class SynapseDate(DateEntity):
+class SynapseDate(SynapseBaseEntity, DateEntity):
     def __init__(
         self,
         hass: HomeAssistant,
-        hub: SynapseBridge,
+        bridge: SynapseBridge,
         entity: SynapseDateDefinition,
     ):
-        self.hass = hass
-        self.bridge = hub
-        self.entity = entity
+        super().__init__(hass, bridge, entity)
         self.logger = logging.getLogger(__name__)
-        self._listen()
 
-    # common to all
-    @property
-    def device_info(self) -> DeviceInfo:
-        """Return device registry information for this entity."""
-        declared = self.entity.get("device_id", "")
-        if len(declared) > 0:
-            return self.bridge.device_list[declared]
-        return self.bridge.device
-
-    @property
-    def unique_id(self):
-        return self.entity.get("unique_id")
-
-    @property
-    def suggested_object_id(self):
-        return self.entity.get("suggested_object_id")
-
-    @property
-    def translation_key(self):
-        return self.entity.get("translation_key")
-
-    @property
-    def icon(self):
-        return self.entity.get("icon")
-
-    @property
-    def extra_state_attributes(self):
-        return self.entity.get("attributes") or {}
-
-    @property
-    def entity_category(self):
-        if self.entity.get("entity_category") == "config":
-            return EntityCategory.CONFIG
-        if self.entity.get("entity_category") == "diagnostic":
-            return EntityCategory.DIAGNOSTIC
-        return None
-
-    @property
-    def name(self):
-        return self.entity.get("name")
-
-    @property
-    def suggested_area_id(self):
-        return self.entity.get("area_id")
-
-    @property
-    def labels(self):
-        return self.entity.get("labels")
-
-    @property
-    def available(self):
-        return self.bridge.connected()
-
-    # domain specific
     @property
     def native_value(self):
         return date.fromisoformat(self.entity.get("native_value"))
@@ -114,31 +42,3 @@ class SynapseDate(DateEntity):
             self.bridge.event_name("set_value"),
             {"unique_id": self.entity.get("unique_id"), "value": value, **kwargs},
         )
-
-    def _listen(self):
-        self.async_on_remove(
-            self.hass.bus.async_listen(
-                self.bridge.event_name("update"),
-                self._handle_entity_update,
-            )
-        )
-        self.async_on_remove(
-            self.hass.bus.async_listen(
-                self.bridge.event_name("health"),
-                self._handle_availability_update,
-            )
-        )
-
-    @callback
-    def _handle_entity_update(self, event):
-        if event.data.get("unique_id") == self.entity.get("unique_id"):
-            self.entity = event.data.get("data")
-            self.async_write_ha_state()
-
-    @callback
-    async def _handle_availability_update(self, event):
-        """Handle health status update."""
-        self.async_schedule_update_ha_state(True)
-
-
-# type: ignore
